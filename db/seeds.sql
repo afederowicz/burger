9f22b9aae5baa920bceab59667bce4cc25d542c4
Andrew Federowicz <afederowicz@Andrews-iMac.fios-router.home>
2017-03-24 15:34:20 -0400

    set up file trees and most ofd most of the document content

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devouered) VALUES ('Truffle Burger', false);
+INSERT INTO burgers (name, devouered) VALUES ('Bacon Supreme Burger', false);
+INSERT INTO burgers (name, devouered) VALUES ('Turkey Burgers Arent Real Burgers', false);
